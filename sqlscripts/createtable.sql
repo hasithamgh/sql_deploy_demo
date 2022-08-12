7e5e73c22b7ab760d3a4d0a20f94ebda0cfa0044
Hasitha Chinthaka <hasitha.chinthaka@gmail.com>
2022-08-11 20:56:12 -0600

    Create createtable.sql

---
+++ sqlscripts/createtable.sql
@@ -0,0 +1,13 @@
+CREATE TABLE [dbo].[Product](
+	[ProductID] [int] IDENTITY(1,1) PRIMARY KEY NOT NULL,
+	[Name] [nvarchar](100) NOT NULL,
+	[ProductNumber] [nvarchar](25) NOT NULL,
+	[Color] [nvarchar](15) NULL,
+	[StandardCost] [money] NOT NULL,
+	[ListPrice] [money] NOT NULL,
+	[Size] [nvarchar](5) NULL,
+	[Weight] [decimal](8, 2) NULL,
+	[ProductCategoryID] [int] NULL,
+	[ProductModelID] [int] NULL,
+	[ModifiedDate] [datetime] NOT NULL
+)
