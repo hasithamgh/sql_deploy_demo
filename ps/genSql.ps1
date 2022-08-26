b7f58b185102b247c48652e437a60ab84f724722
Hasitha Chinthaka <hasitha.chinthaka@gmail.com>
2022-08-26 15:26:44 -0600

    db update

--- ps/genSql.ps1
+++ ps/genSql.ps1
@@ -11,6 +11,27 @@ foreach ($row in $file)
         Write-Host $row.Fname
         Write-Host $row.Lname
         Write-Host $row.ID
+
+        $serverName = "LAPTOP4F1K83KS"
+$databaseName = "StudentDetails"
+$tableName = "[dbo].[Demotbl]"
+$studentName = 'John','Debo','Carry','Mini'
+$standard = '5'
+$Connection = New-Object System.Data.SQLClient.SQLConnection
+$Connection.ConnectionString = "Server=tcp:adfskdemo.database.windows.net,1433;Initial Catalog=adfdemo;Persist Security Info=False;User ID=adfadmin;Password=Password@01;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+$Connection.Open()
+$Command = New-Object System.Data.SQLClient.SQLCommand
+$Command.Connection = $Connection
+foreach($Name in $studentName){
+  $insertquery="
+  UPDATE [dbo].[Demotbl]
+   SET [Fname] = '$row.Fname'
+      ,[Lname] = '$row.Lname'
+ WHERE ID = 1"
+  $Command.CommandText = $insertquery
+  $Command.ExecuteNonQuery()
+}
+$Connection.Close();
     
 
 }
